08a654eab1c4e8d184587a8a74cfedc6dbcb6682
urinboynaziraliyev <naziraliyevurinboy1996@gmail.com>
2021-08-20 16:48:44 +0500

    changed data can not loaded

--- app/src/main/java/com/example/todosimple/AdapterHelper.java
+++ app/src/main/java/com/example/todosimple/AdapterHelper.java
@@ -11,60 +11,46 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class AdapterHelper  {
-
-   private Context mContext;
-   private TaskAdapter taskAdapter;
-
+public class AdapterHelper extends RecyclerView.Adapter<AdapterHelper.MyHolderview>  {
+    private Context context;
+    private  List<ToDoModel> list = new ArrayList<>();
 
 
-    public void setConfig(RecyclerView recyclerView,Context context,List<ToDoModel> mToDoModelList,List<String>  mKeys){
-       mContext =context;
-       taskAdapter = new TaskAdapter(mToDoModelList,mKeys);
-       recyclerView.setLayoutManager(new LinearLayoutManager(context));
-       recyclerView.setAdapter(taskAdapter);
-   }
+    public AdapterHelper(Context context, List<ToDoModel> list) {
+        this.context = context;
+        this.list = list;
+    }
 
-    public class MyHolderView extends RecyclerView.ViewHolder {
-        private TextView task,date;
-        private String key;
+    @Override
+    public MyHolderview onCreateViewHolder( ViewGroup parent, int viewType) {
 
-        public MyHolderView(View itemView) {
+        return new MyHolderview(LayoutInflater.from(context).inflate(R.layout.item_view,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder( MyHolderview holder, int position) {
+
+        ToDoModel item = list.get(position);
+        holder.task.setText(item.task);
+        holder.date.setText(item.date);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class MyHolderview extends RecyclerView.ViewHolder {
+        TextView task,date;
+        public MyHolderview( View itemView) {
             super(itemView);
             task = itemView.findViewById(R.id.task);
             date = itemView.findViewById(R.id.date);
-        }
-        public void bind(ToDoModel toDoModel, String key){
-                  task.setText(toDoModel.getTask());
-                  date.setText(toDoModel.getDate());
-                  this.key = key;
 
-        }
-    }
-   class TaskAdapter extends RecyclerView.Adapter<MyHolderView> {
-        private List<ToDoModel> taskList;
-        private List<String> keys;
-
-       public TaskAdapter(List<ToDoModel> taskList, List<String> keys) {
-           this.taskList = taskList;
-           this.keys = keys;
-       }
-
-       @Override
-        public MyHolderView onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new MyHolderView(parent);
-        }
-
-        @Override
-        public void onBindViewHolder( AdapterHelper.MyHolderView holder, int position) {
-            holder.bind(taskList.get(position),keys.get(position));
-        }
-
-        @Override
-        public int getItemCount() {
-            return taskList.size();
         }
     }
 }
--- app/src/main/java/com/example/todosimple/MainActivity.java
+++ app/src/main/java/com/example/todosimple/MainActivity.java
@@ -1,141 +1,185 @@
 package com.example.todosimple;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.todosimple.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-
-import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private EditText taskedit;
-    List<ToDoModel> toDoModelList;
-    List<String> list;
-    DatabaseReference myRef;
+    private AdapterHelper adapterHelper;
+    private DatabaseReference myRef;
+    private FirebaseDatabase mFirebaseInstance;
+    private Calendar calander;
+    private SimpleDateFormat simpleDateFormat;
+    private List<ToDoModel> toDoModelList = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         taskedit = findViewById(R.id.edit_task);
         recyclerView = findViewById(R.id.recyclerView_task);
-        toDoModelList = new ArrayList<>();
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Tasks");
-
-//        myRef.setValue("Hello, World!");
-
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
 
+        if (toDoModelList.size()>0){
+        loadedData(taskedit.getText().toString());
+        }else {
+            Toast.makeText(this, "Can not find Any data", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    private void insertedToFirebase() {
-        ToDoModel task = new ToDoModel();
-        task.setTask(taskedit.getText().toString());
-        new FirebaseHelper().AddTask(task, new FirebaseHelper.DataStatus() {
-            @Override
-            public void DataIsloaded(List<ToDoModel> list, List<String> keys) {
+    private void showNotification() {
 
-            }
+        // intent triggered, you can add other intent for other actions
+        Intent i = new Intent(this, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, i, 0);
+        Uri urinotify = null;
+        //Notification sound
+        try {
+            urinotify = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), urinotify);
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-            @Override
-            public void DataIsInserted() {
-                Toast.makeText(MainActivity.this, "Data is add to firebase ", Toast.LENGTH_SHORT).show();
-            }
+        // this is it, we'll build the notification!
+        // in the addAction method, if you don't want any icon, just set the first param to 0
+        Notification mNotification = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
 
-            @Override
-            public void DataIsUpdate() {
+            mNotification = new Notification.Builder(this)
 
-            }
+                    .setContentTitle("Wings-Traccar!")
+                    .setContentText("You are punched-in for more than 10hrs!")
+                    .setContentIntent(pIntent)
+                    .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                    .setSound(urinotify)
+                    .build();
 
-            @Override
-            public void DataIsDelete() {
+        }
 
-            }
-        });
-    }
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-    private void loadedFromFirebase() {
-        new FirebaseHelper().ReadData(new FirebaseHelper.DataStatus() {
-            @Override
-            public void DataIsloaded(List<ToDoModel> list, List<String> keys) {
-                new AdapterHelper().setConfig(recyclerView,MainActivity.this,list,keys);
-            }
+        // If you want to hide the notification after it was selected, do the code below
+        // myNotification.flags |= Notification.FLAG_AUTO_CANCEL;
 
-            @Override
-            public void DataIsInserted() {
-
-            }
-
-            @Override
-            public void DataIsUpdate() {
-
-            }
-
-            @Override
-            public void DataIsDelete() {
-
-            }
-        });
+        notificationManager.notify(0, mNotification);
     }
 
     public void AddTask(View view) {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("Tasks");
-        String ts = taskedit.getText().toString();
-        myRef.setValue(ts);
-        readData();
-       // AdapterHelper adapterHelper = new AdapterHelper.TaskAdapter(toDoModelList,list);
-//        insertedToFirebase();
-//        loadedFromFirebase();
+
+        calander = Calendar.getInstance();
+        simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String currentTime = simpleDateFormat.format(calander.getTime());
+
+        String task = taskedit.getText().toString();
+        if (!task.equals("")) {
+            writeDataToFirebase(task, currentTime);
+            loadedData(task);
+        } else {
+            Toast.makeText(this, "Please write to task place", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 
-    private void readData() {
-        ValueEventListener postListener = new ValueEventListener() {
+    private void loadedData(String task) {
+        Toast.makeText(this, "Data is loading ", Toast.LENGTH_LONG).show();
+        final DatabaseReference nm= FirebaseDatabase.getInstance().getReference().child("Tasks").child(task);
+        nm.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // Get Post object and use the values to update the UI
-                ToDoModel post = dataSnapshot.getValue(ToDoModel.class);
-                toDoModelList.add(post);
-                Toast.makeText(MainActivity.this, "Data is loaded", Toast.LENGTH_SHORT).show();
+
+                if (dataSnapshot.exists()){
+
+                    for (DataSnapshot npsnapshot : dataSnapshot.getChildren()){
+                        ToDoModel l=npsnapshot.getValue(ToDoModel.class);
+                        toDoModelList.add(l);
+                        Toast.makeText(MainActivity.this, "successfully loaded", Toast.LENGTH_SHORT).show();
+                    }
+                    adapterHelper=new AdapterHelper(MainActivity.this,toDoModelList);
+                    recyclerView.setAdapter(adapterHelper);
+
+                }else {
+                    Toast.makeText(MainActivity.this, "Data is not exist", Toast.LENGTH_SHORT).show();
+                }
+
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                // Getting Post failed, log a message
-                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+
             }
-        };
-        myRef.addValueEventListener(postListener);
+        });
     }
+
+
+    private void writeDataToFirebase(String task, String currentTime) {
+
+        myRef = FirebaseDatabase.getInstance().getReference();
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!snapshot.child("Tasks").child(task).exists()) {
+                  HashMap<String,String> myMap = new HashMap<>();
+                  myMap.put("task",task);
+                  myMap.put("time",currentTime);
+                  myRef.child("Task").child(task).setValue(myMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                      @Override
+                      public void onComplete(@NonNull Task<Void> task) {
+                          Toast.makeText(MainActivity.this, "Success "+task, Toast.LENGTH_SHORT).show();
+                      }
+                  });
+
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Your data is already exist ...", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
 }
